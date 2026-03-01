31762c14f60734d14ec286b78df343eb075cc668
Thanh Nguyen <nhatthanh510@gmail.com>
2026-03-01 11:06:47 +1100

    boost screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import { Navbar, Welcome, Dock, Desktop } from '@/components'
 import { Terminal, Finder, Safari, Contact, Gallery, ImageViewer, PDFViewer, TextViewer } from '@/windows'
+import { BootScreen } from '@/components/BootScreen'
 import { Draggable } from "gsap/Draggable";
 import gsap from "gsap";
 
 gsap.registerPlugin(Draggable);
 
 function App() {
+  const [booted, setBooted] = useState(false)
+
+  if (!booted) {
+    return <BootScreen onComplete={() => setBooted(true)} />
+  }
+
   return (
-    <main>
+    <main className="animate-fade-in">
       <Navbar />
       <Welcome />
       <Desktop />
